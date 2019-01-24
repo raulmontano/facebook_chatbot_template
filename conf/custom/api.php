2ba64fad589dd3294215764dab550eeead6a70e5
Fran Caballero <fcaballero@ip-192-168-240-43.eu-west-1.compute.internal>
2019-01-24 11:28:13 +0100

    Upload configuration to debug in Heroku

--- conf/custom/api.php
+++ conf/custom/api.php
@@ -1,6 +1,11 @@
 <?php
 
 return array(
-	'key' 		=> '',
-	'secret'	=> ''
+    // fcaballero_chatbot_en
+	// 'key' 		=> 'HbTOBJgZ8Ge/ROoQBPKMkDQaDBYYkqqoq3btDz4Gx0A=',
+	// 'secret'	=> 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0IjoiZmNhYmFsbGVyb19jaGF0Ym90X2VuIn0.ZRwH8xI5MXWyhUUSw0gYDPMgNYlplTNtvU0ZEUH0J3Pz-hFA6c9xrExfs8zsoVUPnsExeXlN9iqFpa2jSWK9ZQ'
+
+    // xapo_chatbot_en
+    'key'       => 'i6q2qyyxw/+G2yy74JBtOUApMCjcIE5gzbBNEfLTg4s=',
+    'secret'    => 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJwcm9qZWN0IjoieGFwb19jaGF0Ym90X2VuIn0.I5x2-OIHMyyM6-KhLaelUz-gETx87gyImyjHQ5Oe0ZK25izGY1qNfncftjLIa8Wtf2XvaVGZHogOMY9ytEYfcw',
 );
